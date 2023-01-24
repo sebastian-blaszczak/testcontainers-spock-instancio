392be96db8d7f7d0f897e613d1533d43bfb01f15
Sebastian Błaszczak <sebastian.blaszczak.1994@gmail.com>
2023-01-24 20:26:55 +0100

    Add junit calculator test

---
+++ src/test/java/com/codepole/testcontainersspockinstancio/calculator/PriceCalculatorTest.java
@@ -0,0 +1,45 @@
+package com.codepole.testcontainersspockinstancio.calculator;
+
+import com.codepole.testcontainersspockinstancio.item.ItemDto;
+import com.codepole.testcontainersspockinstancio.item.ItemType;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = PriceCalculator.class)
+class PriceCalculatorTest {
+
+    @Autowired
+    private PriceCalculator calculator;
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "SHIRT, 50, SPECIAL_CODE_15, 40",
+                    "BELT, 50, WRONG_CODE, 45",
+                    "SHIRT, 50, WRONG_CODE, 50",
+                    "SOCKS, 50, {}, 50",
+            },
+            emptyValue = "{}")
+    void shouldProperlyCalculatePrice(ItemType type, Double price, String code, Double afterDiscount) {
+        // given
+        List<ItemDto> items = List.of(ItemDto.builder()
+                .price(price)
+                .type(type)
+                .build());
+
+        // when
+        Optional<Double> resultPrice = calculator.calculate(items, code);
+
+        // then
+        assertThat(resultPrice).isPresent().contains(afterDiscount);
+    }
+
+
+}
